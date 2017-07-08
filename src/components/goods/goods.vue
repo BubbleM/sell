e300b9bfa4ffaadc032895778efd51a4667bda88
BubbleM <1870408473@qq.com>
2017-07-08 14:40:17 +0800

    add goods_right css style

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -12,9 +12,9 @@
     <div class="foods-wrapper">
       <ul>
         <li v-for="item in goods" class="food-list">
-          <h1 class="titile">{{item.name}}</h1>
+          <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" class="food-item">
+            <li v-for="food in item.foods" class="food-item border-1px">
               <div class="icon">
                 <img :src="food.icon"/>
               </div>
@@ -22,12 +22,12 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}份</span>
+                  <span class="count">月售{{food.sellCount}}份</span>
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>${{food.price}}</span>
-                  <span v-show="food.oldPrice">${{food.oldPrice}}</span>
+                  <span class="now">${{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">${{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -39,6 +39,7 @@
 </template>
 
 <script type="text/ecmascript-6">
+
   const ERR_OK = 0;
   export default {
     props: {
@@ -111,4 +112,52 @@
           font-size: 12px
     .foods-wrapper
       flex: 1
+      .food-list
+        .title
+          padding-left: 14px
+          height: 26px
+          line-height: 26px
+          border-left: 2px solid #d9dde1
+          font-size: 12px
+          color: rgb(147,153,159)
+          background: #f3f5f7
+        .food-item
+          display: flex
+          margin: 18px
+          padding-bottom: 18px // 去除上下margin有重叠效果
+          border-1px(rgba(7,17,27,0.1))
+          &:last-child
+            border-none()
+            margin-bottom: 0  // 最后一个　否则她会有margin18px+padding18px即36px
+          .icon
+            flex: 0 0 57px
+            margin-right: 10px
+          .content
+            flex: 1
+            .name
+              margin: 2px 0 8px 0
+              height: 14px
+              line-height: 14px
+              font-size: 14px
+              color: rgb(7,17,27)
+            .desc, .extra
+              line-height: 10px
+              font-size: 10px
+              color: rgb(147,153,159)
+            .desc
+              margin-bottom: 8px
+            .extra
+              &.count
+                margin-right: 12px
+            .price
+              font-weight: 700
+              line-height: 24px
+              .now
+                margin-right: 8px
+                font-size: 14px
+                color: rgb(240,20,20)
+              .old
+                text-decoration: line-through
+                font-size: 10px
+                color: rgb(147,153,159)
 </style>
