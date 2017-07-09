8d6d76e04310b128eb398b6e82a4af290825992e
BubbleM <1870408473@qq.com>
2017-07-09 21:24:10 +0800

    add cartcontrol_goods animate

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0"
-    @click="decreaseCart"></div>
+    <!--外层动画-->
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0"
+           @click="decreaseCart">
+        <!--内层动画-->
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addCart"></div>
   </div>
@@ -47,12 +53,26 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .cartcontrol
     font-size: 0
-    .cart-decrease, .cart-add
+    .cart-decrease　// 外层动画原始状态
       display: inline-block
       padding: 6px // 增加点击区域
-      line-height: 24px
-      font-size: 24px
-      color: rgb(0,160,220)
+      opacity: 1
+      transform: translate3D(0, 0, 0) // 硬件加速,让动画更流畅
+      transition: all 0.4s linear
+      .inner
+        display: inline-block
+        line-height: 24px
+        font-size: 24px
+        color: rgb(0,160,220)
+        transition: all 0.4s linear
+        transform: rotate(0)
+      &.move-enter-active, &.move-leave-active
+        transition: all 0.4s linear
+      &.move-enter, &.move-leave
+        opacity: 0
+        transform: translate3D(24px, 0, 0)
+        .inner
+          transform: rotate(180deg)
     .cart-count
       display: inline-block
       vertical-align: top
@@ -64,4 +84,8 @@
       color: rgb(147,153,159)
     .cart-add
       display: inline-block
+      padding: 6px // 增加点击区域
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0,160,220)
 </style>
