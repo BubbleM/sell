dbd8df457ae1a6aea8761bb2e50b8f14ab9af4ac
BubbleM <1870408473@qq.com>
2017-07-14 20:17:47 +0800

    add food_info

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -26,6 +26,11 @@
             </div>
           </transition>
         </div>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
       </div>
     </div>
   </transition>
@@ -35,6 +40,7 @@
   import BScroll from 'better-scroll';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
   import Vue from 'vue';
+  import split from 'components/split/split';
 
   export default {
     props: {
@@ -72,7 +78,8 @@
       }
     },
     components: {
-      cartcontrol
+      cartcontrol,
+      split
     }
   };
 </script>
@@ -164,4 +171,16 @@
         &.fade-enter, &.fade-leave-active
           opacity: 0
           z-index: -1
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rgb(7,17,27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77,85,93)
 </style>
