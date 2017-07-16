7c0b8111e81e3a228908f2c862651bc02879e63e
BubbleM <1870408473@qq.com>
2017-07-16 15:33:04 +0800

    update border none bug

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -193,7 +193,7 @@
           flex: 1
           text-align: center
           border-right: 1px solid rgba(7,17,27,0.1)
-          &.last-child
+          &:last-child
             border: none
           h2
             margin-bottom: 4px
