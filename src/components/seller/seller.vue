9e9464d85184032ca233b4ef4501dc6bf8886547
BubbleM <1870408473@qq.com>
2017-07-16 14:45:44 +0800

    update seller favorite

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -28,6 +28,10 @@
             </div>
           </li>
         </ul>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{'active':favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
+        </div>
       </div>
       <split></split>
       <div class="bulletin">
@@ -40,6 +44,24 @@
             <span class="icon" :class="classMap[seller.supports[index].type]"></span>
             <span class="text">{{seller.supports[index].description}}</span>
           </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picList">
+            <li class="pic-item" v-for="pic in seller.pics">
+              <img :src="pic" width="120" height="90"/>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <spilt></spilt>
+      <div class="info">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item" v-for="info in seller.infos">{{info}}</li>
         </ul>
       </div>
     </div>
@@ -55,6 +77,16 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data() {
+      return {
+        favorite: false
+      };
+    },
+    computed: {
+      favoriteText() {
+        return this.favorite ? '已收藏' : '收藏';
       }
     },
     created() {
@@ -76,6 +108,12 @@
       });
     },
     methods: {
+      toggleFavorite(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.favorite = !this.favorite;
+      },
       _initScroll() {
         if (!this.scroll) {
           this.scroll = new BScroll(this.$refs.seller, {
@@ -83,6 +121,24 @@
           });
         } else {
           this.scroll.refresh();
+        }
+      },
+      _initPics() {
+        if (this.seller.pics) {
+          let picWidth = 120;
+          let margin = 6;
+          let width = (picWidth + margin) * this.seller.pics.length - margin;
+          this.$refs.picList.style.width = width + 'px';
+          this.$nextTick(() => {
+            if (!this.picScroll) {
+              this.picScroll = new BScroll(this.$refs.picWrapper, {
+                scrollX: true,
+                eventPassthrough: 'vertical' // 忽略纵向的滚动
+              });
+            } else {
+              this.picScroll.refresh();
+            }
+          });
         }
       }
     },
@@ -104,6 +160,7 @@
     width: 100%
     overflow: hidden
     .overview
+      position: relative
       padding: 18px
       .title
         margin-bottom: 8px
@@ -145,6 +202,24 @@
             color: rgb(7, 17, 27)
             .stress
               font-size: 24px
+      .favorite
+        position: absolute
+        width: 50px
+        right: 11px
+        top: 18px
+        text-align: center
+        .icon-favorite
+          display: block
+          margin-bottom: 4px
+          line-height: 24px
+          font-size: 24px
+          color: #d4d6d9
+          &.active
+            color: rgb(240,20,20)
+        .text
+          line-height: 10px
+          font-size: 10px
+          color: rgb(77, 85, 93)
     .bulletin
       padding: 18px 18px 0 18px
       .title
@@ -164,6 +239,8 @@
           padding: 16px 12px
           border-1px(rgba(7, 17, 27, 0.1))
           font-size: 0
+          &:last-child
+            border-none()
         .icon
           display: inline-block
           vertical-align: top
@@ -186,4 +263,39 @@
           line-height: 16px
           font-size: 12px
           color: rgb(7, 17, 27)
+    .pics
+      padding: 18px
+      .title
+        margin-bottom: 12px
+        line-height: 14px
+        color: rgb(7, 17, 27)
+        font-size: 14px
+      .pic-wrapper
+        width: 100%
+        overflow: hidden
+        white-space: nowrap // 不折行
+        .pic-list
+          font-size: 0
+          .pic-item
+            display: inline-block
+            margin-right: 6px
+            width: 120px
+            height: 90px
+            &:last-child
+              margin: 0
+    .info
+      padding: 18px 18px 0 18px
+      color: rgb(7, 17, 27)
+      .title
+        padding-bottom: 8px
+        line-height: 14px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 14px
+      .info-item
+        padding: 16px 12px
+        line-height: 16px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size: 12px
+        &:last-child
+          border-none() // 把最后一个1px的border去掉
 </style>
